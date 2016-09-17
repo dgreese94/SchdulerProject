43d7bbb9f21a08f66b68dbef8fff9b93cc17ebba
Dylan Reese <dylangreese@gmail.com>
2016-09-17 13:32:29 -0400

    New scheduler changes

--- process.h
+++ process.h
@@ -13,8 +13,11 @@
 #define MAX_THREADS 8
 #define TIMESPEC_NS 1000000
 #define TIMESPEC_SEC 0
-#define HIGHEST_PRIORITY 10
+#define SCHEDULER_PRIORITY 10
+#define HIGHEST_PRIORITY 9
+#define LOWEST_PRIORITY (3)
 #define IDLE_PRIORITY 2
+#define DONE_PRIORITY (1)
 
 typedef struct process_args {
 	unsigned int exec_time;
